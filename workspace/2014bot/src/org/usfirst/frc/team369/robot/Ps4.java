a964126e5ed01c7f1b0c548425aef889dcc8d78c
ShadowShitler <Ronny10m@gmail.com>
2017-01-05 08:52:28 -0500

    Update Ps4.java

--- workspace/2014bot/src/org/usfirst/frc/team369/robot/Ps4.java
+++ workspace/2014bot/src/org/usfirst/frc/team369/robot/Ps4.java
@@ -19,6 +19,37 @@ public class Ps4 extends Joystick{
 		if(this.getRawButton(button) && !past.get(button)){
 			return true;
 		}
+		
 		return false;
 	}
+	
+	static class buttons{
+		static int 
+		square = 1,
+		x = 2,
+		circle = 3,
+		triangle = 4,
+		L1 = 5,
+		R1 = 6,
+		L2 = 7,
+		R2 = 8,
+		share = 9,
+		options = 10,
+		L3 = 11,
+		R3 = 12,
+		ps = 13,
+		touchpad = 14,
+		//analog
+		leftX = 0,
+		leftY = 1,
+		rightX = 2,
+		L2Analog = 3,
+		R2Analog = 4,
+		rightY = 5;
+	}
+//	public double getAxis(int axis){
+//		this.getRawAxis(axis) && !past.get(axis){
+//			return;
+//		}
+//	}
 }
